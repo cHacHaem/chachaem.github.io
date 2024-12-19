796afe0b924fd7932f2e781b85946241c70e5608
cHacHaem <113141138+cHacHaem@users.noreply.github.com>
2024-12-18 20:21:27 -0500

    Update script.js

--- script.js
+++ script.js
@@ -7,12 +7,6 @@ const jsConfetti = new JSConfetti();
         emojis: ['🌈', '⚡️', '💥', '✨', '💫', '🌸', '😆', '😊', '🏵️', '🎃', '🍓', '🍉', '🍒'],
     })
 });
-caution.addEventListener("click", () => {
-   body.style.background = "url('https://cdn.glitch.global/9e4c7eb7-23b2-40d4-a4e2-de43f02bf3a3/barney_jumpscare.png?v=1702662320151')";
-  setTimeout(() => {
-  body.style.background = "";
-}, 5000);
-});
 function searchText(input, data) {
     const query = input.toLowerCase();
     return data.filter(item => item.toLowerCase().includes(query));
